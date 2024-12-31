ab6231fa6da9e6669c76dcddda7e4726b0653ba1
mmirkovicGD <mmirkovic@griddynamics.com>
2024-12-31 08:32:09 +0100

    use RouterProvider in App.tsx insted of index.tsx

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,7 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Twitter from "./pages/twitter/Twitter";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/twitter",
+    element: <Twitter />,
+  },
+]);
 
 function App() {
-  return <Login></Login>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- twitter-clone/src/index.tsx
+++ twitter-clone/src/index.tsx
@@ -3,34 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Signup from "./pages/signup/Signup";
-import Login from "./pages/login/Login";
-import Twitter from "./pages/twitter/Twitter";
 import { UserProvider } from "./context/UserContext";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/twitter",
-    element: <Twitter />,
-  },
-]);
 
 ReactDOM.render(
   <React.StrictMode>
     <UserProvider>
-      <RouterProvider router={router}></RouterProvider>
+      <App />
     </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
